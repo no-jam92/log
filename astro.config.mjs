1ee80d184141164dfa28973f6622da80b41f036c
Jaemin Ahn <nathan.ahn@kurlycorp.com>
2026-01-08 16:39:57 +0900

    feat: integrate with sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte(), sitemap()]
 });
