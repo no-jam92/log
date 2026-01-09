11f7da053302c164c12a8b0fab1d0dc342dae71f
Jaemin Ahn <nathan.ahn@kurlycorp.com>
2026-01-09 14:02:22 +0900

    style: setup for pandacss

--- src/components/GNB.svelte
+++ src/components/GNB.svelte
@@ -1,13 +1,31 @@
 <script lang="ts">
+	import { css } from "@style/css";
+
 	const links: { href: string; label: string }[] = [
 		{ href: "/log", label: "Home" },
 		{ href: "/log/about", label: "About" },
 		{ href: "/log/rss", label: "RSS" },
 	];
+
+	const navStyle = css({
+		display: "flex",
+		justifyContent: "space-between",
+		alignItems: "center",
+		padding: "1rem",
+		maxWidth: "1280px",
+		margin: "0 auto",
+		backgroundColor: "amber.200",
+	});
+
+	const ulStyle = css({
+		listStyle: "none",
+		display: "flex",
+		gap: "1rem",
+	});
 </script>
 
-<nav>
-	<ul>
+<nav class={navStyle}>
+	<ul class={ulStyle}>
 		{#each links as link}
 			<li><a href={link.href}>{link.label}</a></li>
 		{/each}
